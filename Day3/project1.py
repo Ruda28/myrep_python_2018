f568398976b1a73aefbae9a5523b029f3557761c
Andzia <anna.katarzyna.ruta@gmail.com>
2018-06-04 19:56:11 +0200

    normalize user's input

--- Day3/project1.py
+++ Day3/project1.py
@@ -1,6 +1,9 @@
 ADMIN_USERNAME = 'Piotr'
 name=input('Podaj imie')
-if name==ADMIN_USERNAME
+
+name_capitalized=name.lower().capitalize()
+
+if name_capitalized==ADMIN_USERNAME:
     print('hey',name)
 else:
     print ('hey poklikasz?')
